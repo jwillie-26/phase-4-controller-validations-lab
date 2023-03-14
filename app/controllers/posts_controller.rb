c18e789208cc734ca054e6a8b7ad371e74e64ddd
Jesse <jessewambu@zetech.ac.ke>
2023-03-14 10:10:04 +0300

    Updated soln

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,35 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def index
+    posts = Post.all
+    render json: posts
+  end
 
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
+    post.update!(post_params)
+    render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 
-    post.update(post_params)
-
+  def create
+    post = Post.create(post_params)
     render json: post
   end
 
   private
+
+  def render_not_found_response
+    render json: { error: "Post not Found" }, status: :not_found
+  end
 
   def post_params
     params.permit(:category, :content, :title)
